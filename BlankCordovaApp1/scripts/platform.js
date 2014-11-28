bdceb86e43a690dd6a91cd4488882ed2c27b3b90
SK Dutta <skd@officeclip.com>
2014-11-28 18:28:55 -0500

    added spurious alert

--- BlankCordovaApp1/scripts/platform.js
+++ BlankCordovaApp1/scripts/platform.js
@@ -1,4 +1,5 @@
 ﻿$(document).on('deviceready', function () {
+    alert("entered platform.js");
     if (window.device && parseFloat(window.device.version) >= 7.0) {
         alert("found ios");
         $('body').addClass('iOS7');
