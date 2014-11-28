6df5d9891821de91a1fc7aa1b68db850f1491cb9
SK Dutta <skd@officeclip.com>
2014-11-28 17:40:51 -0500

    testing with alert

--- BlankCordovaApp1/merges/ios/scripts/platform.js
+++ BlankCordovaApp1/merges/ios/scripts/platform.js
@@ -1,5 +1,6 @@
 ﻿$(document).on('deviceready', function () {
     if (window.device && parseFloat(window.device.version) >= 7.0) {
+        alert("found ios");
         $('body').addClass('iOS7');
     }
 });
